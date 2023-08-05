46e250dcb9e0a0805169b4867717a014f418bccf
gam-354 <gam_354@hotmail.com>
2023-08-05 11:15:00 +0200

    Allow multiple requests and concatenate setlists

--- setlistsfm.py
+++ setlistsfm.py
@@ -3,27 +3,44 @@ import requests
 import credentials
 import time
 
-SETLISTSFM_API_KEY = credentials.get_credential("setlistsFmApiKey")
-SETLISTSFM_API_URL = "https://api.setlist.fm/rest/1.0/"
+API_KEY = credentials.get_credential("setlistsFmApiKey")
+API_URL = "https://api.setlist.fm/rest/1.0/"
 
-SETLISTSFM_API_COMMAND_SEARCH_ARTIST = "search/artists"
-SETLISTSFM_API_COMMAND_ARTIST = "artist/"
-SETLISTSFM_API_COMMAND_SETLISTS = "setlists/"
+API_COMMAND_SEARCH_ARTIST = "search/artists"
+API_COMMAND_ARTIST = "artist/"
+API_COMMAND_SETLISTS = "setlists/"
+
+API_MIN_SECONDS_BETWEEN_REQUESTS = 0.6
+API_SETLISTS_PER_PAGE = 20
 
 
-
-SETLISTSFM_API_DEFAULT_HEADERS = {
+API_DEFAULT_HEADERS = {
     'Accept' : 'application/json',
-    'x-api-key' : SETLISTSFM_API_KEY
+    'x-api-key' : API_KEY
 }
 
+timeLastRequest = 0.0
 
-def send_get_request(url, params={}, headers=SETLISTSFM_API_DEFAULT_HEADERS):
+def send_get_request(url, params={}, headers=API_DEFAULT_HEADERS):
 
+    global timeLastRequest
+
+    # Ensure it's been enough time  so that a new request can be executed
+    timeFromLastRequest = time.time() - timeLastRequest
+
+    if timeFromLastRequest < API_MIN_SECONDS_BETWEEN_REQUESTS:
+        remainingTime = API_MIN_SECONDS_BETWEEN_REQUESTS - timeFromLastRequest
+        print(f"Waiting {remainingTime} to send request.")
+        time.sleep(remainingTime)
+
+    # Build and execute the request
     response = requests.get(url, params=params, headers=headers)
-    responseJson = response.json()
-
     print(f"Sending GET request: {response.url}")
+
+    # Update the timestamp
+    timeLastRequest = time.time()
+
+    responseJson = response.json()
     print(f"Response: {responseJson}")
 
     return response
@@ -31,7 +48,7 @@ def send_get_request(url, params={}, headers=SETLISTSFM_API_DEFAULT_HEADERS):
 
 def search_artist(artistName):
 
-    url = SETLISTSFM_API_URL + SETLISTSFM_API_COMMAND_SEARCH_ARTIST
+    url = API_URL + API_COMMAND_SEARCH_ARTIST
 
     payload = {
         'artistName' : artistName,
@@ -40,12 +57,6 @@ def search_artist(artistName):
 
     response = send_get_request(url, params=payload)
 
-    # response = requests.get(url, params=payload, headers=SETLISTSFM_API_DEFAULT_HEADERS)
-    # responseJson = response.json()
-
-    # print(f"Sending GET request: {response.url}")
-    # print(f"Response: {responseJson}")
-
     # Raise exception if status code is not OK
     if response.ok:
         return response.json()['artist'][0]
@@ -53,28 +64,66 @@ def search_artist(artistName):
         raise Exception(f"Could not find artist '{artistName}'")
 
 
-def get_setlists(artistMbId):
+def get_setlists(artistMbId, numSetlistsRequested=30):
 
-    url = SETLISTSFM_API_URL + SETLISTSFM_API_COMMAND_ARTIST + artistMbId + "/" + SETLISTSFM_API_COMMAND_SETLISTS
+    setlists = []
+    page = 0
+    numSetlistsAvailable = 1000
 
-    response = send_get_request(url)
+    while (len(setlists) < numSetlistsRequested) and (len(setlists) < numSetlistsAvailable):
+        page += 1
 
-#    response = requests.get(url, headers=SETLISTSFM_API_DEFAULT_HEADERS)
-    responseJson = response.json()
+        url = API_URL + API_COMMAND_ARTIST + artistMbId + "/" + API_COMMAND_SETLISTS
 
-    if not response.ok:
-        raise Exception(f"Could not retrieve setlists")
+        payload = { 'p' : page }
 
-    number_of_setlists = responseJson['total']
-    print(f"Found {number_of_setlists} setlists")
+        response = send_get_request(url, params=payload)
+        responseJson = response.json()
 
+        if not response.ok:
+            raise Exception(f"Could not retrieve setlists")
+
+        numSetlistsAvailable = responseJson['total']
+        print(f"Found {numSetlistsAvailable} setlists")
+
+        # Read the chunk of setlists and append them to the global list
+        setlists += read_setlists_from_setlists_response(responseJson)
+    
+    
+    #print("########################")
+    #print(f"Num setlists downloaded: {len(setlists)}")
+
+    # Clip to the num of setlists requested, in case more
+    # setlists were downloaded accidentally
+    setlists = setlists[:numSetlistsRequested]
+    #print(f"Num setlists to return: {len(setlists)}")
+
+    print(setlists)
+
+
+def read_setlists_from_setlists_response(responseJson):
+
+    setlists = []
+
+    # Add setlists from these results
+    for setlist in responseJson['setlist']:
+        
+        # Try to retrieve the song list. If not available, skip this setlist
+        try:
+            songlist = setlist['sets']['set'][0]['song']
+        except:
+            continue    
+        
+        #print(songlist)
+        setlists.append(songlist)
+
+    return setlists
 
 
 # DEMO
 if __name__ == '__main__':
     artist = search_artist("King Gizzard")
     print(f"Found artist: {artist['name']}")
-    time.sleep(1)
     get_setlists(artist['mbid'])
 
 
